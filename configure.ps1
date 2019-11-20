912f1d72e00ab09088b3674cdadc408b1227ff86
alexwatto <alexwatto96@gmail.com>
2019-11-20 10:58:33 +0000

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -4,7 +4,7 @@
 install-packageprovider -name nuget -minimumversion 2.8.5.201 -force
 
 ## trust the psgallery
-set-psrepository -name "psgallery" -installationpolicy trusted -force
+set-psrepository -name "psgallery" -installationpolicy trusted
 
 ## installed required packages (note that these must be available int he psgallery)
 install-module xstorage -force
